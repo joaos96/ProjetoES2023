6ac813b3fa9be06707665bd3184987641b8424f0
miguelpalberto <miguel.p.alberto@outlook.com>
2023-06-01 22:05:39 +0100

    janela login

---
+++ Implementacao/src/main/java/PaginaLogin.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PaginaLogin extends JFrame {
+
+    private JPanel painelLogin;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea emailTextArea;
+    private JTextArea passwordTextArea;
+    private JButton loginButton;
+    private JButton sairButton;
+
+    public PaginaLogin(String title) {
+        super(title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelLogin);
+        pack();
+        sairButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                // TODO
+                }
+            });
+    }
+    public static void main(String[] args) {
+        new PaginaLogin("Gestão Jiu-Jitsu").setVisible(true);
+    }
+}
